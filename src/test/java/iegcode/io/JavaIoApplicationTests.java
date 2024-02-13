3f3d15879f2e75bc91dd22425d6e5e7a06c44697
ElGibran17 <ibrahimelgibran17@gmail.com>
2024-02-13 14:03:40 +0700

    first commit

---
+++ src/test/java/iegcode/io/JavaIoApplicationTests.java
@@ -0,0 +1,13 @@
+package iegcode.io;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaIoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
