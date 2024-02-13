1dc64ed1517730b872294836e90def54053ade92
ElGibran17 <ibrahimelgibran17@gmail.com>
2024-02-13 14:51:29 +0700

    update

--- src/test/java/iegcode/io/SmallFileTest.java
+++ src/test/java/iegcode/io/SmallFileTest.java
@@ -10,6 +10,7 @@ import java.nio.file.Path;
 
 public class SmallFileTest {
 
+    // jika ingin menulis file ukuran kecil
     @Test
     void writeSmallFile() throws IOException {
         Path path1 = Path.of("small1.txt");
@@ -20,5 +21,19 @@ public class SmallFileTest {
         Path path2 = Path.of("small2.txt");
         Files.writeString(path2, "Hello World");
         Assertions.assertTrue(Files.exists(path2));
+    }
+
+    // kalo ingin membaca file ukuran kecil
+
+    @Test
+    void readSmallFile() throws IOException {
+        Path path1 = Path.of("small1.txt");
+        byte[] bytes = Files.readAllBytes(path1);
+        String contect1 = new String(bytes);
+        Assertions.assertEquals("Hello World", contect1);
+
+        Path path2 = Path.of("small2.txt");
+        String contect2 = Files.readString(path2);
+        Assertions.assertEquals("Hello World", contect2);
     }
 }
